411f5d9ce9cac846417b7b776238d00b011fb17e
shay peduim <shayp@algotec.co.il>
2021-07-22 18:41:49 +0300

    change js to class

--- toggleSlider.js
+++ toggleSlider.js
@@ -1,55 +1,77 @@
-	var colors = [ 
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16),
-						"#" + Math.floor(Math.random()*16777215).toString(16)
-						];
-		
-		$(document).ready(function() {
-		   
-				var element = document.getElementById("mainToggle");
+class ToggleSlider {
+
+
+
+	constructor() {  }
+	init(p_mainElement)
+	{
+		this.mainElement = p_mainElement;
+		this.mainElement.classList.add("myToggle");
+
+		var sliderLine = document.createElement("div");
+		this.mainElement.classList.add("sliderLine");
+		this.mainElement.appendChild(sliderLine);
+
+		// <div id="mainToggle" class="myToggle"> 
+		//    <div class="sliderLine" >
+		//    </div>
+		// </div>
+
+		//var element = document.getElementById("mainToggle");
+
+		this.colors = [ 
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16),
+			"#" + Math.floor(Math.random()*16777215).toString(16)
+			];
 			   
-			   for (let i = 0; i < 10; i++) {
+		for (let i = 0; i < 10; i++) {
 
-					var smalldiv = document.createElement("div");
-				 smalldiv.classList.add("btnSmall");
-				 smalldiv.style["background-color"] = colors[i];
+			var smalldiv = document.createElement("div");
+			smalldiv.classList.add("btnSmall");
+			smalldiv.style["background-color"] = this.colors[i];
 
-				 var div = document.createElement("div");
-				 div.classList.add("btn");
-				 div.appendChild(smalldiv);
-				 
-				 div.addEventListener("click", myFunction);
-				 element.appendChild(div);
-				}
-			   
-			   var div = document.createElement("div");
-			  div.classList.add("btnToggle");
-			  div.id = "toggleSlider";
-			  div.style.left = "0px";
-			  div.style["background-color"] = colors[0];
-			  element.appendChild(div);
-
-		   
-		});
-		
-
-
-		function myFunction(event) {
-		  var rect = event.target;
-		  //alert (rect.style.top + " " + rect.style.left);
-		  //alert (rect.offsetLeft);
-		  
-		  //alert (rect.offsetLeft/30);
-		  
-		  var slider = document.getElementById("toggleSlider");
-		  slider.style.left = rect.offsetLeft + "px";
-		  slider.style["background-color"] = colors[rect.offsetLeft/30 || 0];
-		  //alert (slider);
+			var div = document.createElement("div");
+			div.classList.add("btn");
+			div.appendChild(smalldiv);
+			
+			div.addEventListener("click", this.clickSlider.bind(this));
+			this.mainElement.appendChild(div);
 		}
+		
+		var div = document.createElement("div");
+		div.classList.add("btnToggle");
+		div.id = "toggleSlider";
+		div.style.left = "0px";
+		div.style["background-color"] = this.colors[0];
+		this.mainElement.appendChild(div);
+
+		this.toggleSliderBtn = div;
+	}
+
+	clickSlider(event) {
+		var rect = event.target;
+		//alert (rect.style.top + " " + rect.style.left);
+		//alert (rect.offsetLeft);
+		
+		//alert (rect.offsetLeft/30);
+		
+		//var slider = document.getElementById("toggleSlider");
+		this.toggleSliderBtn.style.left = rect.offsetLeft + "px";
+		this.toggleSliderBtn.style["background-color"] = this.colors[rect.offsetLeft/30 || 0];
+		//alert (slider);
+	  }
+
+  }
+
+		
+
+
+		
